8e3bcffeb7c0529d1459eff0eaaf69575a430150
velmuzhov <velmuzhov@gmail.com>
2025-01-23 18:55:30 +0300

    brief manual over

--- app008/loading_relationships.py
+++ app008/loading_relationships.py
@@ -1,3 +1,10 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from db import User, Address, engine
-session = Session(bind=enginezaz`zasqqsqq  q`cxcvn``)
+
+
+session = Session(bind=engine)
+
+print(select(Address.email_address).select_from(User).join(User.addresses))
+print()
+print(select(Address.email_address).join_from(User, Address))
